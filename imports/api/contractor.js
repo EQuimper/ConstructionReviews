9810d414b1412d4d96e1082dceba262bcb54971b
EQuimper <quimperemanuel@gmail.com>
2016-07-22 12:15:10 -0600

    Initial Commit

---
+++ imports/api/contractor.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const Contractors = new Mongo.Collection('contractors');
---
+++ imports/ui/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export const App = () => <h1>Hello</h1>;
+
